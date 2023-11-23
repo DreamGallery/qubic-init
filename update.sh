a53d5bebe4eb5cc0c1ba89bc3c0a132ab05ddcaf
DreamGallery <49429799+DreamGallery@users.noreply.github.com>
2023-11-23 19:35:51 +0800

    add update.sh

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Usage ./update.sh $qli-version
+
+if [[ -n "$1" ]]; then
+    version="$1"
+    echo "update to version $version"
+else
+    echo "Please input the version"
+    exit 1
+fi
+
+sudo systemctl stop qli.service
+
+sudo -u qubic bash <<EOF
+cd && wget https://dl.qubic.li/downloads/qli-Client-$version-Linux-x64.tar.gz
+cp qcli/appsettings.json appsetting.json
+cp qcli/qli-Service.sh qli-Service.sh
+rm -rf qcli/*
+tar -xzf qli-Client-$version-Linux-x64.tar.gz -C qcli
+mv appsetting.json qcli/appsetting.json
+mv qli-Service.sh qcli/qli-Service.sh
+rm qli-Client-$version-Linux-x64.tar.gz
+EOF
+
+sudo systemctl start qli.service
+sudo journalctl -u qli.service -f
